d9bf28877203c4079b90d2cf1a216021f2550b25
Dimitar Kolev <dick@ingbg.com>
2015-12-22 22:27:21 +0200

    Improved CLI

--- index.js
+++ index.js
@@ -21,57 +21,43 @@ var _ = require('lodash');
 
 module.exports.run = function(options) {
 	mikser(options)
-		.then(debug)
-		.then(broker)
-		.then(config)
-		.then(runtime)
-		.then(databse)
-		.then(parser)
-		.then(loader)
-		.then(generator)
-		.then(scheduler)
-		.then(manager)
-		.then(compilator)
-		.then(watcher)
-		.then(server)
-		.then(diagnostics)
-		.then((mikser) => {
-			if (cluster.isMaster) {
-				console.log('Mikser: Loaded');
-				mikser.loadPlugins().then(() => {
-					mikser.cli
-						.option('-S, --no-server', 'don\'t run web server to access your generated website')
-						.option('-W, --no-watch', 'don\'t watch your website for changes')
-						.init(true);
-
-					mikser.options = _.defaults({ 
-						watch: mikser.cli.watch, 
-						server: mikser.cli.server
-					}, mikser.options);
-					
-					mikser.debug.resetWatch()
-						.then(mikser.manager.glob)
-						.then(mikser.manager.clean)
-						.then(mikser.compilator.compile)
-						.then(mikser.manager.copy)
-						.then(() => {
-							if (mikser.options.server) {
-								mikser.server.listen();
-							}
-
-							mikser.scheduler.process().then(() => {
-								if (mikser.options.watch) {
-									mikser.watcher.watch();
-								} 
-								if (!mikser.options.server && !mikser.options.watch) {
-									mikser.exit();
-								}
-							});
-						});
+	.then(debug)
+	.then(broker)
+	.then(config)
+	.then(runtime)
+	.then(databse)
+	.then(parser)
+	.then(loader)
+	.then(generator)
+	.then(scheduler)
+	.then(manager)
+	.then(compilator)
+	.then(watcher)
+	.then(server)
+	.then(diagnostics)
+	.then((mikser) => {
+		if (cluster.isMaster) {
+			console.log('Mikser: Loaded');
+			mikser.loadPlugins().then(() => {
+				mikser.cli.init(true);					
+				mikser.debug.resetWatch()
+				.then(mikser.manager.glob)
+				.then(mikser.manager.clean)
+				.then(mikser.compilator.compile)
+				.then(mikser.manager.copy)
+				.then(mikser.server.listen)
+				.then(mikser.scheduler.process)
+				.then(mikser.server.refresh)
+				.then(mikser.watcher.start)
+				.then(() => {
+					if (!mikser.options.server && !mikser.options.watch) {
+						mikser.exit();
+					}
 				});
-			} else {
-				mikser.loadPlugins().then(mikser.joinMaster);
-			}
-		});
+			});
+		} else {
+			mikser.loadPlugins().then(mikser.joinMaster);
+		}
+	});
 };
 
