c3495ffbbe1eb456378f1a80382888df219dbdce
Dimitar Kolev <dick@ingbg.com>
2016-01-03 13:59:23 +0200

    Readme

---
+++ plugins/nunjucks.js
@@ -0,0 +1,40 @@
+'use strict'
+var nunjucks = require('nunjucks');
+
+module.exports = function (mikser, context) {
+	var loader = new nunjucks.FileSystemLoader(mikser.config.layoutsFolder, {watch: false});
+	var engine = new nunjucks.Environment(loader, {autoescape: false});
+
+	if (context) {
+		context.nunjucks = function (source, options) {
+			source = mikser.manager.findSource(source);
+			let template = fs.readFileSync(source, { encoding: 'utf8' });
+			let result = engine.renderString(template, options);
+			return result;
+		}
+	} else {
+		mikser.generator.engines.push({
+			pattern: '**/*.+(nj|nunjucks)',
+			render: function(context) {
+				try {
+					let result = engine.renderString(context.layout.template, context);
+					return result;
+				} catch (err) {
+					let re = /\[Line\s(\d+)/;
+					let result = re.exec(err.message);
+					if (result) {
+						let lineNumber = parseInt(result[1]) + 1;
+						let diagnose = mikser.diagnostics.diagnose(context, lineNumber);
+						diagnose.message = err.message.replace(re, '[Line ' + diagnose.lineNumber).replace('(unknown path) ', '');
+
+						err = new Error('Render error');
+						err.diagnose = diagnose;
+						err.origin = 'nunjucks';
+					}
+					throw err;
+				}
+				console.log(result);
+			}
+		});
+	}
+};
---
+++ plugins/twig.js
@@ -0,0 +1,28 @@
+'use strict'
+
+var Twig = require('twig');
+var twig = Twig.twig;
+
+module.exports = function (mikser, context) {
+	if (context) {
+		context.twig = function (source, options) {
+			source = mikser.manager.findSource(source);
+			var template = twig({
+				data: fs.readFileSync(source, { encoding: 'utf8' })
+			});
+			let result = template.render(context);
+			return result;
+		}
+	} else {
+		mikser.generator.engines.push({
+			pattern: '**/*.twig',
+			render: function(context) {
+				var template = twig({
+					data: context.layout.template
+				});
+				let result = template.render(context);
+				return result;
+			}
+		});
+	}
+};
