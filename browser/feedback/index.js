db52b7e9cbdec9358758108620f780086fe46d65
Taliss <ivvlahov@yahoo.com>
2016-05-31 18:39:59 +0300

    Save error and warnings in local storage

--- browser/feedback/index.js
+++ browser/feedback/index.js
@@ -6,56 +6,36 @@ var S = require('string');
 module.exports = function (mikser) {
 		var port = mikser.config.feedbackPort;
 		var ws = new ReconnectingWebSocket('ws://' + location.host.split(':')[0] + ':' + port);
-
-		if (localStorage.length) {
-			// clear happens after all progress events
-			console.log('CLEAR STORAGE !!!');
-			localStorage.clear();
-		}
 		var messages = {
 			error: [],
 			warning: [],
 			progress: []
 		}
-		// var progressStarted, lastPending;
-
-		// function getPendingNumber(message) {
-		// 	if (S(message).contains('Ramaining time:')) {
-		// 		var pending = S(message).between('Pending:', 'Remaining time:').trim().s;
-		// 	} else {
-		// 		var pending = S(message).between('Pending:').trim().s;
-		// 	}
-		// 	return parseInt(pending);
-		// }
 
 		ws.onmessage = function(event) {
 			var parsedData = JSON.parse(event.data);
 
-			if (messages[parsedData.level]) {
-				messages[parsedData.level].push(parsedData.message);
-				console.log(typeof parsedData.level, typeof JSON.stringify(messages[parsedData.level]));
-				// localStorage.setItem('hello', 'world');
-				localStorage.setItem(parsedData.level, JSON.stringify(messages[parsedData.level]));
-				console.log(localStorage.getItem(parsedData.level), 'value from getter');
+			if (parsedData.message === 'render-started') {
+				console.log('--> started');
+				if (localStorage.length) localStorage.clear();
+				messages.error = [];
+				messages.warning = [];
+				messages.progress = [];
 			}
 
-			// if (parsedData.level === 'progress') {
-			// 	console.log(parsedData);
-			// }
+			messages[parsedData.level].push(parsedData.message);
+			localStorage.setItem(parsedData.level, JSON.stringify(messages[parsedData.level]));
 
-			// if (parsedData.level === 'info') {
-			// 	console.log('%c' + parsedData.message, "color: #336600");
-			// 	if (parsedData.message.indexOf('Generation time:') === 0) {
-			// 		nProgress.done();
-			// 	}
-			// }
+			if (parsedData.message === 'render-finished') {
+				console.log('-->', 'finished');
+			}
 
 			if (parsedData.level === 'warning') {
-				console.warn('%c' + parsedData.message, "color: #CC9900");
+				console.warn('%c' + parsedData.message, "background: #e6e600; color: black");
 			}
 
 			if (parsedData.level === 'error') {
-				console.error('%c' + parsedData.message, "color: black");
+				console.error('%c' + parsedData.message, "background: #e62e00; color: black");
 			}
 		}
 
--- plugins/feedback.js
+++ plugins/feedback.js
@@ -4,20 +4,23 @@ let WebSocketServer = require('ws').Server;
 let Promise = require('bluebird');
 let cluster = require('cluster');
 let net = require('net');
+let chalk = require('chalk');
 
 module.exports = function (mikser) {
 	let debug = mikser.debug('feedback');
+	let feedback = {
+		server: {
+			broadcast: () => {}
+		}
+	}
 
 	if (cluster.isMaster) {
-
 		if (mikser.config.feedback === false) {
 			console.log('Feedback is disabled');
 			return Promise.resolve();
 		}
 
 		mikser.config.browser.push('feedback');
-		var feedback = {};
-
 		mikser.cleanup.push(() => {
 			if (feedback.server) {
 				debug('Closing web socket server');
@@ -31,12 +34,12 @@ module.exports = function (mikser) {
 
 		mikser.on('mikser.server.ready', () => {
 			feedback.server = new WebSocketServer({ port: mikser.config.feedbackPort });
-			mikser.config.feedbackIsRunning = true;
 
 			feedback.server.broadcast = function broadcast(data) {
 				if (typeof data !== 'string') {
 					data = JSON.stringify(data);
 				}
+
 				feedback.server.clients.forEach(function each(client) {
 					client.send(data, (err) => {
 						if (err) {
@@ -53,7 +56,13 @@ module.exports = function (mikser) {
 					debug(`Feedback disconnected.Code: ${code}`, message);
 				});
 			});
+		});
 
+		mikser.on('mikser.scheduler.renderStarted', () => {
+			console.log(chalk.bgCyan.bold('Started'));
+			return feedback.server.broadcast({
+				message: 'render-started'
+			});
 		});
 
 		mikser.on('mikser.diagnostics.progress', (progress) => {
@@ -66,7 +75,16 @@ module.exports = function (mikser) {
 				return feedback.server.broadcast(message);
 			}
 		});
+
+		mikser.on('mikser.scheduler.renderFinished', () => {
+			console.log(chalk.bgGreen.bold('Finished'));
+			return feedback.server.broadcast({
+				message: 'render-finished'
+			});
+		});
+
 	}
+
 
 	mikser.on('mikser.diagnostics.log', (log) => {
 		if (log.level !== 'info') {
@@ -77,12 +95,12 @@ module.exports = function (mikser) {
 
 			if (cluster.isMaster && feedback.server) {
 				return feedback.server.broadcast(data);
-			} else if(cluster.isWorker && mikser.config.feedbackIsRunning) {
+			} else if(cluster.isWorker) {
 				return mikser.broker.call('mikser.plugins.feedback.server.broadcast', data);
 			}
 		}
-
 	});
+
 
 	if (cluster.isWorker) {
 		return Promise.resolve();
