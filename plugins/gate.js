d43745d12507cd5ab9148e87b2aee3561de97ef4
Dimitar Kolev <dick@ingbg.com>
2016-07-08 16:59:58 +0300

    Improved gate and and added plugin event

--- plugins/gate.js
+++ plugins/gate.js
@@ -14,65 +14,69 @@ let base32 = require('base32')
 module.exports = function(mikser) {
 	if (cluster.isWorker || mikser.config.gate === false) return;
 	let debug = mikser.debug('gate');
+	mikser.options.gate = {};
 
 	function showServerInfo() {
+		for(let portName of _.keys(mikser.options.gate)) {
+			if (portName != 'server') mikser.diagnostics.log('info', 'Gate[' + portName + ']: http://' + 'm' + base32.encode(mikser.options.gate[portName]) + '.mikser.io/');
+		}
 		if (mikser.config.shared.length) {
 			for (let share of mikser.config.shared) {
-				mikser.diagnostics.log('info', 'Gate: http://' + 'm' + base32.encode(mikser.options.gate) + '.mikser.io' + S(share).ensureLeft('/').s);
+				mikser.diagnostics.log('info', 'Gate: http://' + 'm' + base32.encode(mikser.options.gate['server']) + '.mikser.io' + S(share).ensureLeft('/').s);
 			}
 		}
 		else {
-			mikser.diagnostics.log('info', 'Gate: http://' + 'm' + base32.encode(mikser.options.gate) + '.mikser.io/');
+			mikser.diagnostics.log('info', 'Gate: http://' + 'm' + base32.encode(mikser.options.gate['server']) + '.mikser.io/');
 		}
 	};
 
-	mikser.on('mikser.server.ready', () => {
-		reconnect({
-			strategy: 'fibonacci',
-			failAfter: Infinity
-		}, (connection) => {
-			let mx = MuxDemux((stream) => {
-				if (stream.meta.tunnel) {
-					stream.pipe(net.connect({port: mikser.config.serverPort})).pipe(stream).on('error', debug);					
-				}
-			});
-			connection.pipe(mx).pipe(connection).on('error', debug);
-			mx.createStream({
-				gate: mikser.options.gate
-			}).end();
-		}).connect({
-			port: 9090,
-			host: 'mikser.io'
-		}).on('error', debug);
+	function openGate(port, portName) {
+		let recent = path.join(mikser.config.runtimeFolder, 'recent', 'gate.json');
+		return fs.existsAsync(recent).then((exists) => {
+			if (exists) {
+				return fs.readJsonAsync(recent, 'utf-8');
+			} else {
+				return {};
+			}
+		}).then((gateInfo) => {
+			if (gateInfo[portName]) {
+				mikser.options.gate[portName] = gateInfo[portName];
+				return Promise.resolve();
+			}
+			mikser.options.gate[portName] = shortid.generate();
+			return fs.outputJsonAsync(recent, mikser.options.gate);
+		}).then(() => {
+			reconnect({
+				strategy: 'fibonacci',
+				failAfter: Infinity
+			}, (connection) => {
+				let mx = MuxDemux((stream) => {
+					if (stream.meta.tunnel) {
+						stream.pipe(net.connect({port: port})).pipe(stream).on('error', debug);					
+					}
+				});
+				connection.pipe(mx).pipe(connection).on('error', debug);
+				mx.createStream({
+					gate: mikser.options.gate[portName]
+				}).end();
+			}).connect({
+				port: 9090,
+				host: 'mikser.io'
+			}).on('error', debug);
+		});			
+	}
 
-		showServerInfo();
+	mikser.on('mikser.server.ready', () => {
+		return openGate(mikser.config.serverPort, 'server').then(() => showServerInfo());
 	});
 
 	mikser.on('mikser.scheduler.renderFinished', () => {
 		if (!_.keys(mikser.server.clients).length) showServerInfo();
 	});
 
+	mikser.on('mikser.utils.resolvePort', openGate);
 
-	if (mikser.config.gate && shortid.isValid(mikser.config.gate)) {
-		mikser.options.gate = mikser.config.gate;
-		return Promise.resolve();
+	return {
+		open: openGate
 	}
-	let recent = path.join(mikser.config.runtimeFolder, 'recent', 'gate.json');
-	return fs.existsAsync(recent).then((exists) => {
-		if (exists) {
-			return fs.readJsonAsync(recent, 'utf-8');
-		} else {
-			return {};
-		}
-	}).then((gateInfo) => {
-		if (gateInfo.gate) {
-			mikser.options.gate = gateInfo.gate;
-			return Promise.resolve();
-		}
-		mikser.options.gate = shortid.generate();
-		return fs.outputJsonAsync(recent, {
-			gate: mikser.options.gate
-		});
-	});
-
 }
