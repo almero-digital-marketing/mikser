7c30f8ffe09cf68c67bd4fb471444b3d3b20b7e1
Dimitar Kolev <dick@ingbg.com>
2016-07-12 14:16:33 +0300

    Type guessing support in ArchieML

--- plugins/archieml.js
+++ plugins/archieml.js
@@ -1,17 +1,39 @@
 'use strict'
 
 var archieml = require('archieml');
+let _ = require('lodash');
 
 module.exports = function (mikser, context) {
+	function customizer(value) {
+		if (_.isString(value)) {
+			let trimmedValue = value.trim();
+
+			let float = parseFloat(trimmedValue);
+			let int = _.parseInt(trimmedValue);
+			if (!_.isNaN(float) || !_.isNaN(int)) {
+				if (float == int) return int;
+				else return float;
+			} 
+
+			if (trimmedValue.toLowerCase() == 'false') return false;
+			if (trimmedValue.toLowerCase() == 'true') return true;
+			
+			let date = Date.parse(trimmedValue);
+			if (!_.isNaN(date)) return date;
+		}
+	}
+
 	if (context) {
 		context.archieml = function (content) {
-			return archieml.load(content);
+			let raw = archieml.load(content);
+			return _.cloneDeepWith(raw, customizer);
 		}
 	} else {
 		mikser.parser.engines.push({
 			pattern: '**/*.+(aml)', 
 			parse: function(content) {
-				return archieml.load(content);
+				let raw = archieml.load(content);
+				return _.cloneDeepWith(raw, customizer);
 			}
 		});
 	}
