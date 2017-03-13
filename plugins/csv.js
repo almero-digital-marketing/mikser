38dfdaadde748199008f71e6883fdab97ea39f8a
Dimitar Kolev <dick@ingbg.com>
2017-03-13 12:38:09 +0200

    Csv plugin

---
+++ plugins/csv.js
@@ -0,0 +1,17 @@
+'use strict'
+
+var csv = require('csv-parse');
+
+module.exports = function (mikser, context) {
+	if (context) {
+		context.csv = function (content, options) {
+			return csv.parse(content, options);
+		}
+	} else {
+		mikser.on('mikser.manager.importDocument', (document) => {
+			if (document.sourceExt == '.csv') {
+				document.records = csv.parse(document.content, mikser.config.csv);
+			}
+		});
+	}
+};
